49ecee7e18aa72ca5094d78ef0a54fd2d58c538e
HuangFru <68625618+HuangFru@users.noreply.github.com>
2022-11-18 20:03:49 +0800

    Wait for MySQL initialized before launching service (#4)
    * images rebuild and changes for conf
    
    * ams use mysql as metadata database
    
    * Add a shell script to start AMS after mysql is already started and initialized.
    
    * Add a shell script to start AMS after mysql is already started and initialized.
    
    * Add a shell script to start AMS after mysql is already started and initialized.
    
    * Update docker/playground-ams-entrypoint.sh
    
    * Add a shell script to start AMS after mysql is already started and initialized.
    
    Co-authored-by: Cheng Pan <pan3793@gmail.com>

--- docker/playground-ams-entrypoint.sh
+++ docker/playground-ams-entrypoint.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-sleep 20
+
+SQL_RESULT=""
+while [[ "${SQL_RESULT}" != "ddl_record" ]]
+do
+	echo "Wait for table initialization to complete..."
+	sleep 1
+	SQL_RESULT=$(mysql -h mysql -P 3306 --database=arctic \
+	  --user="${MYSQL_USER}" --password="${MYSQL_PASSWORD}" \
+	  --execute="show tables like 'ddl_record';" \
+	  -s -N)
+done
+echo "Successfully get the last table created by arctic-init.sql:" "${SQL_RESULT}"
+echo "MySQL initialization is successful, starting AMS..."
+
 /opt/arctic/bin/ams.sh start
