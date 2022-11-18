46efaf8331b267f793e500c73a7fc0068186a927
Cheng Pan <pan3793@gmail.com>
2022-11-18 17:44:26 +0800

    Update docker/playground-ams-entrypoint.sh

--- docker/playground-ams-entrypoint.sh
+++ docker/playground-ams-entrypoint.sh
@@ -7,7 +7,8 @@ do
 	sleep 1
 	SQL_RESULT=$(mysql -h mysql -P 3306 --database=arctic \
 	  --user="${MYSQL_USER}" --password="${MYSQL_PASSWORD}" \
-	  --execute="show tables like 'ddl_record';" -s -N)
+	  --execute="show tables like 'ddl_record';" \
+	  -s -N)
 done
 echo "Successfully get the last table created by arctic-init.sql:" "${SQL_RESULT}"
 echo "Mysql initialization is successful, starting AMS..."
